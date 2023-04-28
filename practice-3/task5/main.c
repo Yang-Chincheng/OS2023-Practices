7b409dcae097feeab94a45857c88e1c973f8fe30
Modem514 <indigea514@gmail.com>
2023-04-28 13:30:06 +0800

    feat(fs): implement tasks

--- practice-3/task5/main.c
+++ practice-3/task5/main.c
@@ -3,6 +3,8 @@
 #include <unistd.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+
 int main(){
     
     pid_t pid;
@@ -11,25 +13,23 @@ int main(){
     fd = open("test.txt" , O_RDWR | O_CREAT | O_TRUNC);
     if (fd == -1)
     {
-        /* code */
+        perror("fail on open test.txt\n");
+		return -1;
     }
     //write 'hello fcntl!' to file
-
-    /* code */
-
-    
+    if (write(fd, "hello fcntl!", 12) < 0) {
+        perror("fail to write to test.txt\n");
+        return -1;
+    }
 
     // DUPLICATE FD
-
-    /* code */
-    
-    
+    int dup_fd = fcntl(fd, F_DUPFD, 0);
 
     pid = fork();
 
     if(pid < 0){
         // FAILS
-        printf("error in fork");
+        perror("error in fork\n");
         return 1;
     }
     
@@ -38,23 +38,68 @@ int main(){
     if(pid > 0){
         // PARENT PROCESS
         //set the lock
-
+        memset(&fl, 0, sizeof(fl));
+        fl.l_type = F_WRLCK;
+        if (fcntl(fd, F_SETLKW, &fl) < 0) {
+            perror("fail to lock file in parent\n");
+            exit(-1);
+        }
         //append 'b'
-        
+        if (write(fd, "b", 1) < 0) {
+            perror("fail to write in parent\n");
+            exit(-1);
+        }
         //unlock
+        fl.l_type = F_UNLCK;
+        if (fcntl(fd, F_SETLKW, &fl) < 0) {
+            perror("fail to release lock in parent\n");
+            exit(-1);
+        }
+
         sleep(3);
 
-        // printf("%s", str); the feedback should be 'hello fcntl!ba'
-        
+        char str[1024];
+        memset(str, 0, sizeof(str));
+        if (lseek(fd, 0, SEEK_SET) < 0) {
+            perror("fail to move r/w pointer\n");
+            exit(-1);
+        }
+        if (read(fd, str, 1024) < 0) {
+            perror("fail to read in parent\n");
+            exit(-1);
+        }
+        // the feedback should be 'hello fcntl!ba'
+        printf("%s\n", str);
+
         exit(0);
 
     } else {
         // CHILD PROCESS
         sleep(2);
         //get the lock
-        
+        if (fcntl(dup_fd, F_GETLK, &fl) < 0) {
+            perror("fail to get flock in child\n");
+            exit(-1);
+        }
         //append 'a'
-
+        if (fl.l_type == F_UNLCK) {
+            fl.l_type = F_WRLCK;
+            if (fcntl(dup_fd, F_SETLKW, &fl) < 0) {
+                perror("fail to lock file in child\n");
+                exit(-1);
+            }
+            if (write(dup_fd, "a", 1) < 0) {
+                perror("fail to write in child\n");
+                exit(-1);
+            }
+            fl.l_type = F_UNLCK;
+            if (fcntl(dup_fd, F_SETLKW, &fl) < 0) {
+                perror("fail to release lock in child\n");
+                exit(-1);
+            }
+        }
+        else 
+            printf("the file is locked in child\n");
         exit(0);
     }
     close(fd);
--- practice-3/task6/main.cpp
+++ practice-3/task6/main.cpp
@@ -7,16 +7,17 @@
 #include <iostream>
 #include <sys/socket.h>
 #include <arpa/inet.h>
- 
- 
+
 using namespace std;
- 
+
 int main()
 {	
 	//得到套接字描述符
 	int sockfd;		
-
-    /* code */
+    if ((sockfd = socket(AF_INET, SOCK_DGRAM, 0)) < 0) {
+        cerr << "open sock error" << endl;
+        exit(-1);
+    }
 	
 	struct ifconf ifc;
 	caddr_t buf;
@@ -26,14 +27,16 @@ int main()
 	ifc.ifc_len = 1024;
 	if ((buf = (caddr_t)malloc(1024)) == NULL)
 	{
-		cout << "malloc error" << endl;
+		cerr << "malloc error" << endl;
 		exit(-1);
 	}
 	ifc.ifc_buf = buf; 
 	
 	//获取所有接口信息
-	
-    /* code */
+	if (ioctl(sockfd, SIOCGIFCONF, &ifc) < 0) {
+        cerr << "get ifconf error" << endl;
+        exit(-1);
+    }
 	
 	//遍历每一个ifreq结构
 	struct ifreq *ifr;
@@ -45,21 +48,26 @@ int main()
 		cout << "interface name: "<< ifr->ifr_name << endl;
 		//ipv4地址
 		cout << "inet addr: " 
-			 << inet_ntoa(((struct sockaddr_in*)&(ifr->ifr_addr))->sin_addr)
-			 << endl;
+            << inet_ntoa(((struct sockaddr_in*)&(ifr->ifr_addr))->sin_addr)
+            << endl;
 		
 		//获取广播地址
 		ifrcopy = *ifr;
-
-		/* code */
+        if (ioctl(sockfd, SIOCGIFBRDADDR, &ifrcopy) < 0) {
+            cerr << "get broad address error" << endl;
+            exit(-1);
+        }
 
 		cout << "broad addr: "
-			 << inet_ntoa(((struct sockaddr_in*)&(ifrcopy.ifr_addr))->sin_addr)
-			 << endl;
+            << inet_ntoa(((struct sockaddr_in*)&(ifrcopy.ifr_addr))->sin_addr)
+            << endl;
+
 		//获取mtu
 		ifrcopy = *ifr;
-		
-        /* code */
+		if (ioctl(sockfd, SIOCGIFMTU, &ifrcopy) < 0) {
+            cerr << "get mtu error" << endl;
+            exit(-1);
+        }
 
 		cout << "mtu: " << ifrcopy.ifr_mtu << endl;
 		cout << endl;
